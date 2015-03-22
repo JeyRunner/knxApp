b29e23049c24c72fbecd82a4adca419bbbfd8bd7
JeyRunner <joshua@johannson.org>
2015-03-22 19:58:04 +0100

    client: android apk setup

---
+++ clinent/android/src/com/pancake/knxApp/knxApp.java
@@ -0,0 +1,30 @@
+package com.pancake.knxApp;
+
+import org.libsdl.app.SDLActivity;
+import android.content.res.AssetManager;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class knxApp extends SDLActivity {
+
+    // load libs
+    static 
+    {
+        // System.loadLibrary("knxAppLib");
+    }
+    
+    public static AssetManager mAssetMgr;
+    
+    // Setup
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        Log.v("SDL", "onCreate()");
+        super.onCreate(savedInstanceState);
+        //mio
+        mAssetMgr = getAssets();
+    }
+    
+}
